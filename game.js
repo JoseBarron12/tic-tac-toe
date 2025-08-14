0573494cd577ce30e31d8f2a14d01271e93f3d21
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-08-13 21:56:21 -0500

    Update createGameBoard() with further gamelogic
    Gameboard object can update array with player moves
    and validate each move to not allow a played space
    to be overridden.

--- game.js
+++ game.js
@@ -1,9 +1,27 @@
 function createGameboard() {
-    const gameBoard = new Array(9);
-    return{gameBoard};
+    let gameBoard = new Array(9);
+
+    const getCurrentGameBoard = () => gameBoard;
+    
+    const addMove = (playerNum, index) => {
+        gameBoard[index] = playerNum;
+    }
+    
+    const validateMove = (index) => {
+        let isValid = true;
+        if (index < 0 || index > gameBoard.length)
+        {
+            isValid = false;
+        }
+        if (gameBoard[index] != undefined)
+        {
+            isValid = false;
+        }
+        return isValid;
+    }
+
+    return{getCurrentGameBoard, addMove, validateMove};
 }
-const board = createGameboard();
-console.log(board.gameBoard);
 
 function createPlayer(playerName, playerNum) {
     let score = 0;
@@ -13,17 +31,8 @@ function createPlayer(playerName, playerNum) {
     return {playerName, playerNum, getScore, updateScore};
 }
 
-const playerOne = createPlayer("Jay", 1);
-const playerTwo = createPlayer("Brown", 2);
+const game = createGameboard();
 
-playerOne.updateScore();
-playerTwo.updateScore();
-playerOne.updateScore();
-playerTwo.updateScore();
-playerOne.updateScore();
-playerTwo.updateScore();
-playerOne.updateScore();
-playerTwo.updateScore();
-
-console.log(playerOne);
-console.log(playerTwo);
+console.log(game.validateMove(5));
+game.addMove(1, 5);
+console.log(game.validateMove(5));
