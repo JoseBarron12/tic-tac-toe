6f3383c02e352b520c8b9efbd6dfc0c01d7e52f9
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-08-13 21:02:05 -0500

    Add createPlayer() for player creation
    Object holds player name, and score in order for
    future game logic to update and get.

--- game.js
+++ game.js
@@ -4,3 +4,23 @@ function createGameboard() {
 }
 const board = createGameboard();
 console.log(board.gameBoard);
+
+function createPlayer(name) {
+    const playerName = "Player: " + name;
+    
+    let score = 0;
+    const getScore = () => score;
+    const updateScore = () => score++;
+
+    return {playerName, getScore, updateScore};
+}
+
+const playerOne = createPlayer("Jay");
+
+playerOne.updateScore();
+playerOne.updateScore();
+
+console.log({
+    playerName: playerOne.playerName,
+    score: playerOne.getScore()
+});
